5dc6bdf7fc0df39f2ea7b397d47966b7a5e63223
luckyz20 <xiang.bean.zhao@gmail.com>
2022-06-02 15:50:19 +0800

    add mini-redis project for leaning

--- my_rust_utils/src/main.rs
+++ my_rust_utils/src/main.rs
@@ -3,10 +3,8 @@ mod zx_utils;
 use zx_utils::http;
 
 fn main() {
-  let resp = http::http_get(&"https://httpbin.org/ip".to_string()).expect("failed");
-  println!("{:#?}", resp);
-  
+    let resp_get = http::http_get(&"https://httpbin.org/ip".to_string()).expect("failed");
+    let resp_post = http::http_post(&"https://httpbin.org/post".to_string()).expect("failed");
+    println!("Get: {:#?}", resp_get);
+    println!("Post: {:#?}", resp_post);
 }
-
-
-
